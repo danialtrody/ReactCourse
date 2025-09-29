721caa54b76d5d9620e5fce3f6ca8d9c67fd0de6
danial <trody2001@gmail.com>
2025-09-29 18:57:52 +0300

    12-keeper-app-part-1-challenge

---
+++ 12-keeper-app-part-1-challenge/src/compenents/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Footer(){
+
+    const date = new Date().getFullYear();
+
+    return(
+        <footer>
+            <p>Copyrightⓒ {date}</p>
+        </footer>
+    )
+}
+
+
+export default Footer;
