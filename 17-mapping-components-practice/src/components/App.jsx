516acdbcd4ab4c617bec69f0f5d6316effe6eb2a
danial <trody2001@gmail.com>
2025-10-04 20:39:38 +0300

    mapping-components-practice

---
+++ 17-mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Entry from "./Entry"
+import emojipedia from "../emojipedia"
+
+
+function emojipediaMap(entry){
+  return (
+    <Entry
+    key={entry.id}
+    emoji={entry.emoji} 
+    name={entry.name}
+    meaning={entry.meaning} 
+   />
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(emojipediaMap)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
